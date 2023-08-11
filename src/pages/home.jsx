dc5ba2d0218e01a4b03ac58a91f9c3a7b9aa2049
yuval-ro <yuvalro@protonmail.com>
2023-08-11 18:59:31 +0300

    Updates to question form

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -39,11 +39,101 @@ const Home = () => {
     setChecked(defaultStates.checked)
   }
 
+  // const handleJsonChange = (newFile) => {
+  //   const allowedTypes = ["application/json"]
+  //   if (newFile === undefined) {
+  //     return
+  //   }
+  //   else if (!allowedTypes.includes(newFile?.type)) {
+  //     alert(`Allowed file types: ${[...allowedTypes]}`)
+  //     setJsonImport(null)
+  //   }
+  //   else {
+  //     setJsonImport(newFile)
+  //   }
+  //   jsonInputRef.current.value = ""
+  // }
+
+  // const handleJsonImport = () => {
+  //   // const reader = new FileReader()
+
+  //   // reader.onload = async (e) => {
+  //   //   const jsonParsed = JSON.parse(e.target.result)
+  //   //   const keys = Object.keys(jsonParsed)
+  //   //   const possibleKeys = ["body", "image", "answers", "corrects", "isRandomized"]
+  //   //   if (!keys.every(key => (possibleKeys.includes(key)))) {
+  //   //     alert("bad JSON!")
+  //   //     return
+  //   //   }
+  //   //   if (jsonParsed.hasOwnProperty("body")) {
+  //   //     setBody(jsonParsed.body)
+  //   //   }
+  //   //   if (jsonParsed.hasOwnProperty("image")) {
+  //   //     const imageName = jsonParsed.image
+  //   //     const storedImageBlob = await getImageFromCache(imageName) // Returns Blob
+
+  //   //     if (storedImageBlob === null) {
+  //   //       alert(`Could not find "${imageName}" in cache`)
+  //   //     }
+  //   //     else {
+  //   //       // const storedImageData = URL.createObjectURL(storedImageBlob)
+  //   //       // const storedImage = new Image()
+  //   //       // storedImage.onload = () => {
+  //   //       //   setImageUrl(storedImage)
+  //   //       // }
+  //   //       // storedImage.src = storedImageData
+  //   //       // setImageUrl(storedImageData)
+  //   //       const storedImageData = URL.createObjectURL(storedImageBlob)
+  //   //       const storedImage = new Image()
+  //   //       storedImage.src = storedImageData
+  //   //       setImageUrl(storedImage)
+  //   //     }
+  //   //   }
+  //   //   if (jsonParsed.hasOwnProperty("answers")) {
+  //   //     setAnswers(jsonParsed.answers)
+  //   //   }
+
+  //   //   if (jsonParsed.hasOwnProperty("corrects")) {
+  //   //     setCorrect(jsonParsed.corrects)
+  //   //   }
+
+  //   //   if (jsonParsed.hasOwnProperty("isRandomized")) {
+  //   //     setShuffle(jsonParsed.isRandomized)
+  //   //   }
+  //   // }
+  //   // reader.readAsText(jsonImport)
+
+  //   // setJsonImport(null)
+  // }
+
+  // const handleJsonExport = () => {
+  //   // const data = {
+  //   //   body,
+  //   //   image: img.name,
+  //   //   answers,
+  //   //   corrects: correct,
+  //   //   isRandomized: shuffle
+  //   // }
+  //   // const json = JSON.stringify(data)
+  //   // const element = document.createElement("a")
+  //   // element.setAttribute("href", "data:text/json;charset=utf-8," + encodeURIComponent(json))
+  //   // // element.setAttribute("download", `${jsonName === "" ? "question" : jsonName}.json`)
+  //   // element.setAttribute("download", `${jsonExport === "" ? "untitled_question" : jsonExport}.json`)
+  //   // element.style.display = "none"
+  //   // document.body.appendChild(element)
+  //   // element.click()
+  //   // document.body.removeChild(element)
+  // }
+
   const ActionBar = () => (
     <Row>
       <Col className="d-flex justify-content-end">
         <Button
-          style={{ cursor: "pointer" }}
+          className="me-auto"
+          variant="light">
+          from JSON
+        </Button>
+        <Button
           variant="outline-danger"
           disabled={checked.length < 1}
           onClick={() => handleRemove()}>
@@ -61,13 +151,12 @@ const Home = () => {
           action
           variant="light"
           style={{ cursor: "default" }}>
-
           <Row>
             <Col xs="1">
               <Form.Check
                 type="checkbox"
                 checked={checked.includes(exam)}
-                onClick={() => handleCheckbox(exam)} />
+                onChange={() => handleCheckbox(exam)} />
             </Col>
             <Col xs="3">
               {exam.metadata.subject}
